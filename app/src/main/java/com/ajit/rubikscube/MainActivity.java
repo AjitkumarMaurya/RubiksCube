ec1982ce43129a605eee3a8453b3f538418a7c62
ajitmaurya <ajitmauray@gmail.com>
2019-06-22 17:23:40 +0530

    Initial commit

---
+++ app/src/main/java/com/ajit/rubikscube/MainActivity.java
@@ -0,0 +1,78 @@
+package com.ajit.rubikscube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv1;
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, getString(R.string.app_id));
+
+        wv1 = findViewById(R.id.webview);
+
+
+        initcode();
+    }
+
+    private void initcode() {
+
+
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // wv1.loadUrl("https://doodlecricket.github.io");
+
+        wv1.loadUrl("file:///android_asset/iframe/index.html");
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.int_ad));
+
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
+
+        // Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                doubleBackToExitPressedOnce=false;
+
+            }
+        }, 2000);
+    }
+}
